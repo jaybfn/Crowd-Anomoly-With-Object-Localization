c2b415122ee2e44b947e7a1250a2be1cfa2d19fa
Tucaa <milantucakovic96@gmail.com>
2022-10-19 18:02:04 +0200

    Added if __name__ block

--- object_detection_image.py
+++ object_detection_image.py
@@ -9,69 +9,73 @@ with open('data\labels\coco.names', 'r') as f:
 
 
 # Object detection for image
+def main():
 
-img = cv2.imread('image.jpg')
-height, width, _ = img.shape
-
-
-blob = cv2.dnn.blobFromImage(img, 1/255, (416, 416), (0,0,0), swapRB=True, crop=False)
-
-net.setInput(blob)
-
-output_layers_names = net.getUnconnectedOutLayersNames()
-layerOuputs = net.forward(output_layers_names)
-
-boxes = []
-confidences = []
-class_ids = []
-
-for output in layerOuputs:
-    for detection in output:
-        scores = detection[5:]
-        class_id = np.argmax(scores)
-        confidence = scores[class_id]
-
-        if confidence > 0.5:
-            center_x = int(detection[0]*width)
-            center_y = int(detection[1]*height)
-            w = int(detection[2]*width)
-            h = int(detection[3]*height)
-
-            x = int(center_x - w/2)
-            y = int(center_y - h/2)
-
-            boxes.append([x, y, w, h])
-            confidences.append(float(confidence))
-            class_ids.append(class_id)
+    img = cv2.imread('image.jpg')
+    height, width, _ = img.shape
 
 
+    blob = cv2.dnn.blobFromImage(img, 1/255, (416, 416), (0,0,0), swapRB=True, crop=False)
 
-#print(len(boxes))
-indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
-#print(indexes.flatten())
+    net.setInput(blob)
 
-font = cv2.FONT_HERSHEY_PLAIN
-colors = np.random.uniform(0, 255, size=(len(boxes), 3))
+    output_layers_names = net.getUnconnectedOutLayersNames()
+    layerOuputs = net.forward(output_layers_names)
 
-for i in indexes.flatten():
+    boxes = []
+    confidences = []
+    class_ids = []
 
-    # Extract information from boxes
-    x, y, w, h, = boxes[i]
+    for output in layerOuputs:
+        for detection in output:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
 
-    # Extract from classes and cast to string as label
-    label = str(classes[class_ids[i]])
+            if confidence > 0.5:
+                center_x = int(detection[0]*width)
+                center_y = int(detection[1]*height)
+                w = int(detection[2]*width)
+                h = int(detection[3]*height)
 
-    # Extract confidance and cast ot string
-    confidence = str(round(confidences[i], 2))
+                x = int(center_x - w/2)
+                y = int(center_y - h/2)
 
-    # Randomly pick color for each object
-    color = colors[i]
-
-    cv2.rectangle(img, (x,y), (x+w, y+h), color, 2)
-    cv2.putText(img, label + " " + confidence, (x, y+20), font, 2, (255,255,255), 2)
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
 
 
 
-cv2.imshow('Image', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    #print(len(boxes))
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+    #print(indexes.flatten())
+
+    font = cv2.FONT_HERSHEY_PLAIN
+    colors = np.random.uniform(0, 255, size=(len(boxes), 3))
+
+    for i in indexes.flatten():
+
+        # Extract information from boxes
+        x, y, w, h, = boxes[i]
+
+        # Extract from classes and cast to string as label
+        label = str(classes[class_ids[i]])
+
+        # Extract confidance and cast ot string
+        confidence = str(round(confidences[i], 2))
+
+        # Randomly pick color for each object
+        color = colors[i]
+
+        cv2.rectangle(img, (x,y), (x+w, y+h), color, 2)
+        cv2.putText(img, label + " " + confidence, (x, y+20), font, 2, (255,255,255), 2)
+
+
+
+    cv2.imshow('Image', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if __name__  ==  "__main__":
+    main()
